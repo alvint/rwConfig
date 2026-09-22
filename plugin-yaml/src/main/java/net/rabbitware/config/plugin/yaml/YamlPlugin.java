90e6d1cee10b40b4005d9b4cf09db509437a3da8
Alvin Thompson <alvin@thompsonlogic.com>
2026-09-22 18:18:16 -0400

    fix missing impot

--- plugin-yaml/src/main/java/net/rabbitware/config/plugin/yaml/YamlPlugin.java
+++ plugin-yaml/src/main/java/net/rabbitware/config/plugin/yaml/YamlPlugin.java
@@ -1,6 +1,7 @@
 package net.rabbitware.config.plugin.yaml;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
